3ba3824cfe81633df9587da2a9da13f3ed7fba61
Lucho <44885834+attrix182@users.noreply.github.com>
2019-05-30 10:37:33 -0300

    Add files via upload

--- Lab/main.c
+++ Lab/main.c
@@ -17,11 +17,12 @@ int main()
     eAlumno unAlumno;
     if((pArchivo = fopen(ARCHIVO, "w"))==NULL)
     {
-        printf("No se puedo leer");
+        printf("No se puedo leer\n");
         exit(1);
-    } else
+    }
+    else
     {
-        printf("se puedo");
+        printf("se puedo\n");
     }
 
     unAlumno.legajo = 1;
@@ -29,11 +30,28 @@ int main()
     strcpy(unAlumno.nombre, "juan");
     fflush(stdin);
 
-    char texto[]="lalala,555,10";
+
+    char texto[]="lalala;555,10";
     int largo = strlen(texto);
     fwrite(texto, sizeof(char),largo,pArchivo);
     fclose(pArchivo);
 
+    if( (pArchivo = fopen(ARCHIVO, "r") )==NULL)
+    {
+        printf("No se puedo leer\n");
+        exit(1);
+    }
+    else
+    {
+        char renglon[70];
+        while(!feof(pArchivo))
+        {
+            int cantidad = fread(renglon, sizeof(char), 70, pArchivo);
+            printf("\nCantidad %d y Texto %s ",cantidad,texto);
+            printf("\n");
+        }
+        fclose(pArchivo);
+    }
 
 
     return 0;
